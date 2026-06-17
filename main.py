da71735e44e2e7b17dcb93f20e9d1318c396b1a5
Den Hafiz <den.m.hafiz@gmail.com>
2026-06-17 22:45:26 +0800

    add:Basic functionality for status

--- main.py
+++ main.py
@@ -2,16 +2,22 @@ from check import get_check
 from alert import get_alert
 from logs import get_logs
 from status import get_status
+from config import get_config
 import sys
 
 
 def main():
     print("This is Updog!")
 
+    get_config()  # Check and create if config did not exist
+
     if len(sys.argv) == 1:
         print("Not enough argument.")
     elif sys.argv[1] == "status":
-        get_status()
+        cpu, ram, disk = get_status()
+        print(f"CPU: {cpu}%")
+        print(f"RAM: {ram}%")
+        print(f"Disk: {disk}%")
     elif sys.argv[1] == "logs":
         get_logs()
     elif sys.argv[1] == "alert":
--- status.py
+++ status.py
@@ -1,2 +1,11 @@
+import psutil
+
+
 def get_status():
     print("This is the status.")
+
+    cpu_percent = psutil.cpu_percent(interval=1)
+    ram_percent = psutil.virtual_memory().percent
+    disk_percent = psutil.disk_usage("/").percent
+
+    return cpu_percent, ram_percent, disk_percent
