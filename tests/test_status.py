683515412c5d652526d58055716a72a058e1b7e0
Den Hafiz <den.m.hafiz@gmail.com>
2026-06-19 07:40:22 +0800

    add:Initial status testing

---
+++ tests/test_status.py
@@ -0,0 +1,15 @@
+from metrics import Metrics
+from status import check_status
+
+
+def test_check_status():
+
+    test1 = Metrics("CPU", 60, 80, 90)
+    test2 = Metrics("CPU", 85, 80, 90)
+    test3 = Metrics("CPU", 91, 80, 90)
+    test4 = Metrics("RAM", 80, 80, 90)
+
+    assert check_status(test1) == "healthy"
+    assert check_status(test2) == "warning"
+    assert check_status(test3) == "critical"
+    assert check_status(test4) == "healthy"
