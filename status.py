f70a7a22768dfb014cf27a6cdaf5660f9b577a99
Den Hafiz <den.m.hafiz@gmail.com>
2026-06-18 20:17:08 +0800

    refactor:Metrics into it's own file

--- status.py
+++ status.py
@@ -1,17 +1,22 @@
-import psutil
+from metrics import get_metrics, Metrics
 from emoji import emojize
+
+
+def check_status(metric: Metrics):
+    # Emoji for showing status
+    check_mark = emojize(":check_mark_button:")
+    exclamation_mark = emojize(":red_exclamation_mark:")
+
+    if metric.current_percentage <= metric.threshold:
+        print(f"[{check_mark}] {metric.name}: {metric.current_percentage}")
+    else:
+        print(f"[{exclamation_mark}] {metric.name}: {metric.current_percentage}")
+    pass
 
 
 def get_status():
     print("This is the status.")
 
-    check_mark = emojize(":check_mark_button:")
-    exclamation_mark = emojize(":red_exclamation_mark:")
+    cpu, ram, disk = get_metrics()
 
-    cpu_percent = psutil.cpu_percent(interval=1)
-    ram_percent = psutil.virtual_memory().percent
-    disk_percent = psutil.disk_usage("/").percent
-
-    print(f"{check_mark}, {exclamation_mark}")
-
-    return cpu_percent, ram_percent, disk_percent
+    check_status(cpu)
