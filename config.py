7b306ce77182228f48d72b78e119a8ee3d3e6f93
Den Hafiz <den.m.hafiz@gmail.com>
2026-06-18 17:47:23 +0800

    refactor:Config file

--- config.py
+++ config.py
@@ -1,17 +1,20 @@
-from pathlib import Path, PosixPath
+from pathlib import Path
 import json
 import os
 
 
 # Default config
-def get_default() -> dict:
+def generate_default(config_file) -> None:
 
     default_config = {
         "cpu_threshold": 80,
         "ram_threshold": 80,
     }
 
-    return default_config
+    with open(config_file, "w") as f:
+        json.dump(default_config, f, indent=4)
+
+    return
 
 
 def get_file_path() -> Path:
@@ -25,17 +28,19 @@ def get_file_path() -> Path:
     return config_file
 
 
-def get_config():
+def get_config() -> dict:
 
     config_file = get_file_path()
-    default_config = get_default()
 
-    # Write to file
-    with open(config_file, "w") as f:
-        json.dump(default_config, f, indent=4)
+    # If config file not exist, fill with default value
+    if not config_file.exists():
+        generate_default(config_file)
 
     # Read from file
     with open(config_file, "r") as f:
         data = json.load(f)
 
     print(f"Data: {data}")
+    print(f"Data type: {type(data)}")
+
+    return data
