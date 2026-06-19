d11b5d996337fdd7dea2277f132106f9df0dd788
Den Hafiz <den.m.hafiz@gmail.com>
2026-06-19 21:53:44 +0800

    add:Print log files with --limit flag

--- logs.py
+++ logs.py
@@ -1,2 +1,12 @@
-def get_logs():
-    print("This is the log.")
+def get_logs(args, limit=5):
+    # Use to print logs. Default is return 5 latest logs
+    # If user use "--limit 2" flag, return latest 2 logs
+
+    if "--limit" in args:
+        limit = int(args[args.index("--limit") + 1])
+
+    with open("updog.log", "r") as f:
+        lines = f.readlines()
+
+    for line in lines[-limit:]:
+        print(line, end="")
