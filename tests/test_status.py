6973e51e05103f76ae68df559027809b52ebaee2
Den Hafiz <den.m.hafiz@gmail.com>
2026-06-19 17:39:59 +0800

    add:Several test cases

--- tests/test_status.py
+++ tests/test_status.py
@@ -4,12 +4,19 @@ from status import check_status
 
 def test_check_status():
 
+    # Normal test cases
     test1 = Metrics("CPU", 60, 80, 90)
     test2 = Metrics("CPU", 85, 80, 90)
     test3 = Metrics("CPU", 91, 80, 90)
+
+    # Edge cases
     test4 = Metrics("RAM", 80, 80, 90)
+    test5 = Metrics("RAM", 90, 80, 90)
+    test6 = Metrics("RAM", 90.1, 80, 90)
 
     assert check_status(test1) == "healthy"
     assert check_status(test2) == "warning"
     assert check_status(test3) == "critical"
     assert check_status(test4) == "healthy"
+    assert check_status(test5) == "warning"
+    assert check_status(test6) == "critical"
