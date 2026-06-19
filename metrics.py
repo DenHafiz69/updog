ec5c4c8e2c2b8ea6734dc16642f3ac74de0add49
Den Hafiz <den.m.hafiz@gmail.com>
2026-06-19 18:22:11 +0800

    refactor:Make it more OOP

--- metrics.py
+++ metrics.py
@@ -1,4 +1,5 @@
 from config import get_config
+from emoji import emojize
 import psutil
 
 
@@ -8,6 +9,36 @@ class Metrics:
         self.current = current
         self.warning = warning
         self.critical = critical
+
+    def check_status(self):
+        if self.current <= self.warning:
+            return 0  # 0 is healthy
+        elif self.current <= self.critical:
+            return 1  # 1 is warning
+        else:
+            return 2  # 2 is critical
+
+    def print_status(self):
+
+        check_mark = emojize(":check_mark_button:")
+        exclamation_mark = emojize(":red_exclamation_mark:")
+        warning = emojize(":warning:")
+
+        status = self.check_status()
+
+        if status == 0:
+            print(f"[{check_mark}] {self.name}: {self.current}% (Healthy)")
+            return "healthy"
+        elif status == 1:
+            print(
+                f"[{warning}] {self.name}: {self.current}% (WARNING: Exceeds {self.warning}% threshold)"
+            )
+            return "warning"
+        else:
+            print(
+                f"[{exclamation_mark}] {self.name}: {self.current}% (CRITICAL: Exceeds {self.critical}% threshold.)"
+            )
+            return "critical"
 
 
 def get_metrics():
--- status.py
+++ status.py
@@ -1,31 +1,9 @@
-from metrics import get_metrics, Metrics
-from emoji import emojize
-
-
-def check_status(metric: Metrics):
-    # Emoji for showing status
-    check_mark = emojize(":check_mark_button:")
-    exclamation_mark = emojize(":red_exclamation_mark:")
-    warning = emojize(":warning:")
-
-    if metric.current <= metric.warning:
-        print(f"[{check_mark}] {metric.name}: {metric.current}% (Healthy)")
-        return "healthy"
-    elif metric.current > metric.warning and metric.current <= metric.critical:
-        print(
-            f"[{warning}] {metric.name}: {metric.current}% (WARNING: Exceeds {metric.warning}% threshold)"
-        )
-        return "warning"
-    else:
-        print(
-            f"[{exclamation_mark}] {metric.name}: {metric.current}% (CRITICAL: Exceeds {metric.critical}% threshold.)"
-        )
-        return "critical"
+from metrics import get_metrics
 
 
 def get_status():
     cpu, ram, disk = get_metrics()
 
-    check_status(cpu)
-    check_status(ram)
-    check_status(disk)
+    cpu.print_status()
+    ram.print_status()
+    disk.print_status()
