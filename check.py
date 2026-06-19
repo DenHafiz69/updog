193514ab8f099bb94c6d16536c3d8ad55964811c
Den Hafiz <den.m.hafiz@gmail.com>
2026-06-19 21:07:32 +0800

    add:Print and save log to updog.log

--- check.py
+++ check.py
@@ -1,2 +1,29 @@
+# Check health and log if needed
+from time import sleep
+import random
+from metrics import get_metrics
+import logging
+
+
+def initialize_logger():
+    logging.basicConfig(
+        filename="updog.log",
+        level=logging.INFO,
+        format="%(asctime)s %(levelname)s %(message)s",
+    )
+
+
 def get_check():
-    print("This is the check.")
+
+    initialize_logger()
+
+    cpu, ram, disk = get_metrics()
+    metrics = [cpu, ram, disk]
+
+    for metric in metrics:
+        if metric.check_status() == 0:
+            logging.info(f"{metric.log()}")
+        elif metric.check_status() == 1:
+            logging.warning(f"{metric.log()}")
+        else:
+            logging.critical(f"{metric.log()}")
