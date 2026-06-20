d613cf81dc29fbee27b94b8633126f56e8069c1c
Den Hafiz <den.m.hafiz@gmail.com>
2026-06-20 21:09:26 +0800

    refactor:Rename config_file_path to avoid confusion

--- config.py
+++ config.py
@@ -2,6 +2,26 @@ from pathlib import Path
 import json
 import os
 
+#
+
+
+def set_config(flag: str, percent: float):
+
+    # User call with flag --set-warning or --set-critical
+    # Then followed by the percentage
+    # warning must be less than critical
+    config_data = get_config()
+    config_file_path = get_file_path()
+
+    print(f"Config: {config_data}")
+
+    # Edit the config here
+    #
+    #
+
+    with open(config_file_path, "w") as f:
+        json.dump(config_file_path, f, indent=4)
+
 
 def generate_default(config_file) -> None:
 
@@ -21,21 +41,21 @@ def get_file_path() -> Path:
         Path(os.environ.get("XDG_CONFIG_HOME", Path.home() / ".config")) / "updog"
     )
     config_dir.mkdir(parents=True, exist_ok=True)
-    config_file = config_dir / "config.json"
+    config_file_path = config_dir / "config.json"
 
-    return config_file
+    # If config file not exist, fill with default value
+    if not config_file_path.exists():
+        generate_default(config_file_path)
+
+    return config_file_path
 
 
 def get_config() -> dict:
 
-    config_file = get_file_path()
-
-    # If config file not exist, fill with default value
-    if not config_file.exists():
-        generate_default(config_file)
+    config_file_path = get_file_path()
 
     # Read from file
-    with open(config_file, "r") as f:
+    with open(config_file_path, "r") as f:
         data = json.load(f)
 
     return data
